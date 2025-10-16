b256e9219f5d087359c4b666835462c46d3394df
Lisa J. Miller <ljmiller@hawaii.edu>
2025-10-15 15:47:37 -1000

    clean up old files

--- src/main/java/orderedlinkedlist/OrderedLinkedListDriver.java
+++ src/main/java/orderedlinkedlist/OrderedLinkedListDriver.java
@@ -1,20 +1,24 @@
 package orderedlinkedlist;
+
 import java.util.PriorityQueue;
 import comparable.ComparableItem;
+
 /**
  * A driver for the generic list of ordered linked nodes
+ * Also tests the PriorityQueue class from java.util.
  * 
+ * @author Lisa Miller
  * @author William McDaniel Albritton
  * @author Edoardo Biagioni
  */
-public class OrderedLinkedListDriver{
+public class OrderedLinkedListDriver {
 
 	/**
 	 * Method main is used as a driver to test the class. Note that this is
 	 * similar to the OrderedArrayList test!
 	 * 
 	 * @param args
-	 *            Not used.
+	 *             Not used.
 	 */
 	public static void main(String[] args) {
 		// test list of Strings
@@ -32,48 +36,48 @@ public class OrderedLinkedListDriver{
 		// test constructor
 		List<ComparableItem> list = new OrderedLinkedList<ComparableItem>();
 		System.out.println(list + "\n");
-      
-      PriorityQueue <ComparableItem> pq = new PriorityQueue<>();
+		//also make a PriorityQueue to compare results
+		PriorityQueue<ComparableItem> pq = new PriorityQueue<>();
 
 		// test add
 		ComparableItem item = new ComparableItem("fork", 4);
 		list.add(item);
-      pq.offer(item);
-		System.out.println(list + "\n");
-      System.out.println(pq + "\n");
+		pq.offer(item);
+		System.out.println("Ordered list: " + list + "\n");
+		System.out.println("Priority queue: " + pq + "\n");
 		item = new ComparableItem("eel", 4);
 		list.add(item);
 		pq.offer(item);
-		System.out.println(list + "\n");
-      System.out.println(pq + "\n");
+		System.out.println("Ordered list: " + list + "\n");
+		System.out.println("Priority queue: " + pq + "\n");
 		item = new ComparableItem("carrot", 6);
 		list.add(item);
 		pq.offer(item);
-		System.out.println(list + "\n");
-      System.out.println(pq + "\n");
+		System.out.println("Ordered list: " + list + "\n");
+		System.out.println("Priority queue: " + pq + "\n");
 		item = new ComparableItem("doughnut", 89);
 		list.add(item);
 		pq.offer(item);
-		System.out.println(list + "\n");
-      System.out.println(pq + "\n");
+		System.out.println("Ordered list: " + list + "\n");
+		System.out.println("Priority queue: " + pq + "\n");
 		item = new ComparableItem("banana", 2);
 		list.add(item);
 		pq.offer(item);
-		System.out.println(list + "\n");
-      System.out.println(pq + "\n");
+		System.out.println("Ordered list: " + list + "\n");
+		System.out.println("Priority queue: " + pq + "\n");
 		item = new ComparableItem("zucchini", 7);
 		list.add(item);
 		pq.offer(item);
-		System.out.println(list + "\n");
-      System.out.println(pq + "\n");
+		System.out.println("Ordered list: " + list + "\n");
+		System.out.println("Priority queue: " + pq + "\n");
 
 		// test remove
 		list.remove(list.size() - 1);
-		System.out.println(list + "\n");
+		System.out.println("Ordered list: " + list + "\n");
 		list.remove(3);
-		System.out.println(list + "\n");
+		System.out.println("Ordered list: " + list + "\n");
 		list.remove(1);
-		System.out.println(list + "\n");
+		System.out.println("Ordered list: " + list + "\n");
 
 		// test exceptions
 		try {
@@ -85,66 +89,66 @@ public class OrderedLinkedListDriver{
 	}// end of main()
 }// end of class
 
-/*PROGRAM OUTPUT:
-1, Chihiro
-2, Kai
-3, Nami
-4, Satsuki
-
-
-
-
-1, fork, 10
-
-
-1, eel, 20
-2, fork, 10
-
-
-1, carrot, 30
-2, eel, 20
-3, fork, 10
-
-
-1, carrot, 30
-2, doughnut, 40
-3, eel, 20
-4, fork, 10
-
-
-1, banana, 50
-2, carrot, 30
-3, doughnut, 40
-4, eel, 20
-5, fork, 10
-
-
-1, banana, 50
-2, carrot, 30
-3, doughnut, 40
-4, eel, 20
-5, fork, 10
-6, zucchini, 60
-
-
-1, banana, 50
-2, carrot, 30
-3, doughnut, 40
-4, eel, 20
-5, fork, 10
-
-
-1, banana, 50
-2, carrot, 30
-3, eel, 20
-4, fork, 10
-
-
-1, carrot, 30
-2, eel, 20
-3, fork, 10
-
-
-ListException: 50 is outside list range. 
+/*
+ * PROGRAM OUTPUT:
+ * 1, Chihiro
+ * 2, Kai
+ * 3, Nami
+ * 4, Satsuki
+ * 
+ * 
+ * 
+ * 
+ * 1, fork, 10
+ * 
+ * 
+ * 1, eel, 20
+ * 2, fork, 10
+ * 
+ * 
+ * 1, carrot, 30
+ * 2, eel, 20
+ * 3, fork, 10
+ * 
+ * 
+ * 1, carrot, 30
+ * 2, doughnut, 40
+ * 3, eel, 20
+ * 4, fork, 10
+ * 
+ * 
+ * 1, banana, 50
+ * 2, carrot, 30
+ * 3, doughnut, 40
+ * 4, eel, 20
+ * 5, fork, 10
+ * 
+ * 
+ * 1, banana, 50
+ * 2, carrot, 30
+ * 3, doughnut, 40
+ * 4, eel, 20
+ * 5, fork, 10
+ * 6, zucchini, 60
+ * 
+ * 
+ * 1, banana, 50
+ * 2, carrot, 30
+ * 3, doughnut, 40
+ * 4, eel, 20
+ * 5, fork, 10
+ * 
+ * 
+ * 1, banana, 50
+ * 2, carrot, 30
+ * 3, eel, 20
+ * 4, fork, 10
+ * 
+ * 
+ * 1, carrot, 30
+ * 2, eel, 20
+ * 3, fork, 10
+ * 
+ * 
+ * ListException: 50 is outside list range.
  */
-
